c60ff01628a755e19150515a2edeb0831dac7512
Pedro Ramos <ramosct70@hotmail.com>
2023-05-15 15:15:17 -0300

    add t1

--- t1/src/Main/Main.java
+++ t1/src/Main/Main.java
@@ -694,7 +694,10 @@ public class Main {
 	public static void main(String[] args) {
 		Database db = new Database();
 		db.createNewDatabase();
-		populateUsers();
+
+		// Descomentar a linha abaixo caso seja necessário adicionar usuários para testes
+		// populateUsers();
+
 		Scanner scan = new Scanner(System.in);
 
 		boolean running = true;
